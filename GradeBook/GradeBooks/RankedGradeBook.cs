f4fa114abdc5280692c6cdb9b72bf4bc63c3c9e8
Bob Peterson <bob@Bobs-MacBook-Pro.local>
2018-11-14 10:00:29 -0600

    Finished creating new gradebook types

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
